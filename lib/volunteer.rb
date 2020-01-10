c12cd1806c4062d8965e11d0da0f3129172876ea
wymerkd <wymerkd@gmail.com>
2020-01-10 13:22:19 -0800

    add method to pass test for #==

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,4 +7,8 @@ class Volunteer
     @project_id = attributes.fetch(:project_id)
     @id = attributes.fetch(:id)
   end
+
+  def ==(volunteer_to_compare)
+    (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+  end
 end
