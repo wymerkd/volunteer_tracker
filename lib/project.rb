22412023d355a0ebc1fa18a8d84d91ed2133216d
wymerkd <wymerkd@gmail.com>
2020-01-10 11:40:28 -0800

    add .all method to pass .all test

--- lib/project.rb
+++ lib/project.rb
@@ -7,6 +7,17 @@ class Project
     @id = attributes.fetch(:id)
   end
 
+  def self.all
+  returned_projects = DB.exec("SELECT * FROM projects;")
+  projects = []
+  returned_projects.each() do |project|
+    name = project.fetch("title")
+    id = project.fetch("id").to_i
+    projects.push(Project.new({:title => title, :id => id}))
+  end
+  projects
+  end
+
   def save
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first().fetch("id").to_i
@@ -15,4 +26,8 @@ class Project
   def ==(other)
   self.class.eql?(other.class) & self.title.eql?(other.title)
   end
+
+  # def self.clear
+  # DB.exec("DELETE FROM projects *;")
+  # end
 end
