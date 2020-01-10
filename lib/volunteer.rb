cb0f66f6aa3f8bae7193d2f77b6d25cf8e20beb3
wymerkd <wymerkd@gmail.com>
2020-01-10 13:19:17 -0800

    initialize attributes for volunteer class to pass test for #name

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,4 +1,10 @@
 class Volunteer
-  def initialize
+  attr_accessor :name, :project_id
+  attr_reader :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
   end
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,13 +1,13 @@
-# require "spec_helper"
-#
-# describe Volunteer do
-#   describe '#name' do
-#     it 'returns the name of the volunteer' do
-#       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       expect(test_volunteer.name).to eq 'Jane'
-#     end
-#   end
-#
+require "spec_helper"
+
+describe Volunteer do
+  describe '#name' do
+    it 'returns the name of the volunteer' do
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      expect(test_volunteer.name).to eq 'Jane'
+    end
+  end
+
 #   describe '#project_id' do
 #     it 'returns the project_id of the volunteer' do
 #       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
@@ -54,4 +54,4 @@
 #       expect(Volunteer.find(volunteer1.id)).to eq volunteer1
 #     end
 #   end
-# end
+end
