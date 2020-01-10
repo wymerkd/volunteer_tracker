a6022b7212fc5caed358dcedb238a1358c2d4c16
wymerkd <wymerkd@gmail.com>
2020-01-10 13:46:00 -0800

    add method to pass .find test

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -13,19 +13,31 @@ class Volunteer
   end
 
   def self.all
-   returned_volunteers = DB.exec("SELECT * FROM volunteers;")
-   volunteers = []
-   returned_volunteers.each() do |volunteer|
-     name = volunteer.fetch("name")
-     project_id = volunteer.fetch("project_id").to_i
-     id = volunteer.fetch("id").to_i
-     volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
-   end
-   volunteers
- end
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
 
- def save
-   result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
-   @id = result.first().fetch("id").to_i
- end
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    if volunteer
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      Volunteer.new({:name => name, :project_id => project_id, :id => id})
+    else
+      nil
+    end
+  end
 end
