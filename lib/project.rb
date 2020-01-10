694fc22e85defc2968bba96a68f8a7168e7a2731
wymerkd <wymerkd@gmail.com>
2020-01-10 10:40:04 -0800

    add code to pass #title test

--- lib/project.rb
+++ lib/project.rb
@@ -1,4 +1,10 @@
 class Project
-  def initialize
+  attr_accessor :title
+
+  # Class variables have been removed.
+
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id) # Note that this line has been changed.
   end
 end
